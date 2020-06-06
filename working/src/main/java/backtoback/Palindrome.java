41e6ca6e2526cc6beedc1f1dba32e05928d0da2a
Maheswari Manoharan <maheswari.manoharan@coxautoinc.com>
2020-06-06 13:26:58 -0400

    Palindrome check for numbers

---
+++ working/src/main/java/backtoback/Palindrome.java
@@ -0,0 +1,122 @@
+package main.java.backtoback;
+
+public class Palindrome {
+
+    /**
+     *
+     * Check If A Number Is A Palindrome
+     * A palindrome is a sequence that reads the same forwards and backward.
+     *
+     * Given an integer as input, write a function that tests if it is a palindrome.
+     *
+     * Example 1:
+     * Input: 9232
+     * Output: false
+     *
+     * Example 2:
+     * Input: 12321
+     * Output: true
+     *
+     * Example 3:
+     * Input: 1
+     * Output: true
+     *
+     * Example 4:
+     * Input: -121
+     * Output: false
+     *
+     * Note
+     * A negative number cannot be a palindrome due to the - sign
+     *
+     * Constraints
+     * You may not cast the number to a string
+     */
+
+/*    class Solution {
+        public boolean isPalindrome(int x) {
+            if (x < 0) {
+                return false; // A negative number can't be symmetric
+            }
+
+            *//*
+             * 10^b = x ... what is b? That is what log base 10 asks.
+             *
+             * 'logarithmAnswer' is b.
+             *//*
+            double logarithmAnswer = Math.log10(x);
+
+            *//*
+             * 'logarithmAnswer' can be a float value. Like:
+             *
+             * log10(99) ≈ 1.99564 (meaning that 10^1.99564 ≈ 99)
+             *
+             * So to get rid of the decimal part we floor it & then add 1
+             *//*
+            int totalDigits = (int) (Math.floor(logarithmAnswer)) + 1;
+
+            *//*
+             * If x = 999, we have 3 digits, so we want a mask the length of the integer.
+             *
+             * 100 = 10^2, so 10^(digits - 1), we will use this later to extract & remove
+             * the most significant digit
+             *//*
+            int mostSignificantExtractionMask = (int) Math.pow(10, totalDigits - 1);
+
+            // Iterate half the total amount of digits, check both ends
+            for (int i = 0; i < (totalDigits / 2); i++) {
+                int mostSignificantDigit = x / mostSignificantExtractionMask;
+                int onesPlaceDigit = x % 10;
+
+                if (mostSignificantDigit != onesPlaceDigit) {
+                    return false;
+                }
+
+                // Removing most significant digit: 999 % 100 = 99
+                x %= mostSignificantExtractionMask;
+
+                *//*
+                 * Removing the ones place digit: 99 / 10 = 9.9 => 9 (.9 truncated since this is
+                 * not a float type, it is an integer)
+                 *//*
+                x /= 10;
+
+                // Remove 2 0's from the mask since we just lost 2 digits
+                mostSignificantExtractionMask /= 100;
+            }
+
+            return true;
+        }
+    }
+    */
+
+    public static void main(String[] args) {
+        Palindrome palindrome = new Palindrome();
+        boolean result = palindrome.isPalindrome(121);
+        System.out.println("is the number a palindrome? " + result);
+    }
+
+    public boolean isPalindrome(int x) {
+        if(x < 0){
+            return false;
+        }
+
+        double logarithmAnswer = Math.log10(x);
+        int totalDigits = (int) (Math.floor(logarithmAnswer) + 1);
+        int mostSignificantExtractionMask = (int) Math.pow(10, totalDigits - 1);
+
+        for(int i = 0; i < (totalDigits / 2); i++) {
+            int mostSignificantDigit = x / mostSignificantExtractionMask;
+            int onesPlaceDigit = x % 10;
+
+            if(mostSignificantDigit != onesPlaceDigit) {
+                return false;
+            }
+
+            x %= mostSignificantExtractionMask;
+            x /= 10;
+
+            mostSignificantExtractionMask /= 100;
+        }
+        return true;
+    }
+}
