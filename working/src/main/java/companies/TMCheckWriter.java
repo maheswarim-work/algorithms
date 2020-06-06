8e280d2d16941d947ccb5eb880bf8682878e793d
Maheswari Manoharan <maheswari.manoharan@coxautoinc.com>
2020-06-06 19:57:17 -0400

    Clean up

--- working/src/main/java/companies/TMCheckWriter.java
+++ working/src/main/java/companies/TMCheckWriter.java
@@ -1,4 +1,9 @@
 package main.java.companies;
 
+/**
+ * A simple console app that will take numerical dollars and cents values and convert them to words that you would write on the Amount line of a check - e.g. “$6.00” to “Six dollars and zero cents”.
+ */
 public class TMCheckWriter {
+
 }
+
